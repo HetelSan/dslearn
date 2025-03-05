07aaee63a7a18c8b11c3e3ca9e2d82c404ae14bc
Hetel Santos <hetel.santos@gmail.com>
2025-03-05 16:13:59 -0300

    Couse

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://png.pngtree.com/background/20240607/original/pngtree-flat-female-cartoon-seminar-with-presenter-training-and-strategy-talk-photo-picture-image_9225031.jpg', 'https://i2.pickpik.com/photos/53/873/273/books-reading-knowledge-read-preview.jpg');
